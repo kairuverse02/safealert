b659b1483d830bcebacc22fd6ed2d0eacb8a1950
Espiritu-Mark-Adrian <espiritumarkadrian@gmail.com>
2026-01-16 12:56:14 +0800

    Add TURN relay servers for cross-network NAT traversal

--- src/app/admin/components/GuardianPairing.tsx
+++ src/app/admin/components/GuardianPairing.tsx
@@ -191,7 +191,23 @@ export default function GuardianPairing({ onRoomCreated, onPairingComplete }: Pr
           { urls: 'stun:stun1.l.google.com:19302' },
           { urls: 'stun:stun2.l.google.com:19302' },
           { urls: 'stun:stun3.l.google.com:19302' },
-          { urls: 'stun:stun4.l.google.com:19302' }
+          { urls: 'stun:stun4.l.google.com:19302' },
+          // Free TURN relay servers for NAT traversal
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
         ],
         iceCandidatePoolSize: 10
       }
