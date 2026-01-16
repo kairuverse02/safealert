67ecbfdd5c75d03e8f6d73473f7721659ae241d5
kairuverse02 <kairusant@gmail.com>
2026-01-16 21:09:33 +0800

    Fix merge conflict in AlertsLog.tsx

--- src/app/admin/components/AlertsLog.tsx
+++ src/app/admin/components/AlertsLog.tsx
@@ -1,73 +1,5 @@
-<<<<<<< HEAD
-// 'use client'
-// import React, { useState } from 'react'
-// import { LogEntry } from "@/types";
-//   const getLogEntryUI = ({ id, time, type, message }: LogEntry) => {
-//     let headerClass, headerText;
-//     switch (type) {
-//       case "sound":
-//         headerClass = "text-yellow-400";
-//         headerText = "SOUND";
-//         break;
-//       case "patient_motion":
-//         headerClass = "text-purple-400";
-//         headerText = "MOTION";
-//         break;
-//       case "bathroom":
-//         headerClass = "text-teal-400";
-//         headerText = "REQUEST";
-//         break;
-//       case "error":
-//         headerClass = "text-red-500";
-//         headerText = "ERROR";
-//         break;
-//       case "sos":
-//         headerClass = "text-red-600 font-extrabold";
-//         headerText = "!!! SOS !!!";
-//         break;
-//       case "info":
-//         headerClass = "text-blue-400";
-//         headerText = "INFO";
-//         break;
-//       case "perimeter":
-//         headerClass = "text-green-400";
-//         headerText = "PERIMETER";
-//         break; // Changed color
-//       default:
-//         headerClass = "text-gray-500";
-//         headerText = "SYSTEM";
-//         break;
-//     }
-//     return (
-//       <p key={id}>
-//         <span className={`font-bold ${headerClass}`}>
-//           [{time}] {headerText}:
-//         </span>{" "}
-//         {message}
-//       </p>
-//     );
-//   };
-=======
 'use client'
 import React from 'react'
->>>>>>> 3bc0c289355ca492d584a751426c87129bcbe296
-
-// const AlertsLog = () => {
-//     const [logEntries] = useState<LogEntry[]>([]);
-//   return (
-//     <div>
-//       <div className='bg-neutral-200 h-[440px]'>
-//         <h1 className='pl-2 text-black font-semibold text-lg '>
-//         Events Log
-//         </h1>
-//       </div>
-//     </div>
-//   )
-// }
-
-<<<<<<< HEAD
-// export default AlertsLog
-=======
 
 const AlertsLog = () => {
   return (
@@ -83,4 +15,3 @@ const AlertsLog = () => {
 }
 
 export default AlertsLog
->>>>>>> 3bc0c289355ca492d584a751426c87129bcbe296
