e23f5123c2ce3c8a4fe7c08f905b5f0e3a3a4d05
kairuverse02 <kairusant@gmail.com>
2026-01-19 11:26:02 +0800

    deleted QRScanner.tsx

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -17,6 +17,7 @@ export default function Home(){
   return (
     <>
     <div>
+      {/* DeletedQRSCANNER */}
       {pairWithId ? (
         <PatientScanner initialRoomId={pairWithId} />
       ) : (
